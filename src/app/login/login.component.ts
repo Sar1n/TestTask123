1b7f8192fb9254a999f7eb1dbeb6ed0cc9752d75
Ivan Maluk <ivan.maluk@gmail.com>
2020-08-07 05:21:48 +0300

    changes to optimisation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,6 @@ import { LoginService } from '../login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  //styleUrls: ['./login.component.css'],
   providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
